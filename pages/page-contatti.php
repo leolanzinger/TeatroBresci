0af99f546f1dc02829d0805dce4579d1e43ff1c7
Juleship <michelon.giulio@gmail.com>
2014-07-07 18:44:59 +0200

    contatti rotti

--- pages/page-contatti.php
+++ pages/page-contatti.php
@@ -4,14 +4,20 @@ Template Name: Contatti Page
 */
 get_header(); ?>
 
-<h1>Contatti</h1>
-<?php
-  if ( have_posts() ) {
-    while ( have_posts() ) {
-      the_post();
-      the_content();
-    }
-  }
-?>
+<div class="container">
+  <h1>Contatti</h1>
+
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php 
+        $post_id = get_the_ID();
+        $tel = get_post_custom_values('email', $post_id); 
+        echo $email[0];
+      ?>
+      <?php the_content(); ?>
+    <?php endwhile; else: ?>
+    <?php endif; ?>
+
+</div>
 
 <?php get_footer(); ?>
