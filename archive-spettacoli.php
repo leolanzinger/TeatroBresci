cb2d7b6b126d9641930a08ea40b03ee7a122dfa3
Juleship <michelon.giulio@gmail.com>
2014-07-10 18:46:06 +0200

    pagina spettacoli finita

--- archive-spettacoli.php
+++ archive-spettacoli.php
@@ -5,45 +5,46 @@
 get_header(); ?>
 
   <div class="container">
-    <h1>Spettacoli</h1>
+    <h1 id="spettacoli-title">Spettacoli</h1>
   </div>
     
   <div id="spettacoli-container">
-    <div class="spettacoli-mask">
-      <h2 class="spettacoli-title">Prosa</h2>
-      <div class="spettacoli-hover">
-        <p>Scopri</p>
+    <a href="<?php echo home_url(); ?>/category/prosa">
+      <div class="spettacoli-mask">
+        <h2 class="spettacoli-title">Prosa</h2>
+        <div class="spettacoli-hover">
+          <p>Scopri</p>
+        </div>
+        <img src="<?php bloginfo( 'template_url' ); ?>/img/home.jpg">
       </div>
-      <img src="<?php bloginfo( 'template_url' ); ?>/img/home.jpg">
-    </div>
-    <div class="spettacoli-mask">
+    </a>
+    <a href="<?php echo home_url(); ?>/category/teatro-ragazzi">
+      <div class="spettacoli-mask">
       <h2 class="spettacoli-title">Teatro Ragazzi</h2>
       <div class="spettacoli-hover">
         <p>Scopri</p>
       </div>
       <img src="<?php bloginfo( 'template_url' ); ?>/img/home.jpg">
-    </div>
-    <div class="spettacoli-mask">
-      <h2 class="spettacoli-title">Commemorativi</h2>
-      <div class="spettacoli-hover">
-        <p>Scopri</p>
       </div>
-      <img src="<?php bloginfo( 'template_url' ); ?>/img/home.jpg">
-    </div>
-    <div class="spettacoli-mask">
+    </a>
+    <a href="<?php echo home_url(); ?>/category/commemorativi">
+      <div class="spettacoli-mask">
+        <h2 class="spettacoli-title">Commemorativi</h2>
+        <div class="spettacoli-hover">
+          <p>Scopri</p>
+        </div>
+        <img src="<?php bloginfo( 'template_url' ); ?>/img/home.jpg">
+      </div>
+    </a>
+    <a href="<?php echo home_url(); ?>/category/lezioni-spettacolo">
+      <div class="spettacoli-mask">
       <h2 class="spettacoli-title">Lezioni spettacolo</h2>
       <div class="spettacoli-hover">
         <p>Scopri</p>
       </div>
       <img src="<?php bloginfo( 'template_url' ); ?>/img/home.jpg">
-    </div>
+      </div>
+    </a>
   </div>
-
-  <ul>
-    <li><a href="<?php echo home_url(); ?>/category/prosa">Prosa</a></li>
-    <li><a href="<?php echo home_url(); ?>/category/teatro-ragazzi">Teatro ragazzi</a></li>
-    <li><a href="<?php echo home_url(); ?>/category/commemorativi">Commemorativi</a></li>
-    <li><a href="<?php echo home_url(); ?>/category/lezioni-spettacolo">Lezioni spettacolo</a></li>
-  </ul>
 
 <?php get_footer(); ?>
