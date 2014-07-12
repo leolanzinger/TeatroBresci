2b39e70bb983ae21fb0e3bf9392f539626c78cda
Juleship <michelon.giulio@gmail.com>
2014-07-12 19:42:48 +0200

    gallery finita

--- pages/page-gallery.php
+++ pages/page-gallery.php
@@ -8,7 +8,11 @@ get_header(); ?>
   <h1>Galleria</h1>
 </div>
 <div id="gallery-instagram">
-  //instagram
+  <div class="container">
+    <h2 id="gallery-instagram-title">Instagram <a href="http://www.instagram.com/teatrobresci">@teatrobresci</a></h2>
+    <!-- SnapWidget -->
+    <iframe src="http://snapwidget.com/bd/?u=dGVhdHJvYnJlc2NpfGlufDIwMHw1fDF8fHllc3wyMHxub25lfG9uU3RhcnR8eWVz&v=7714" title="Instagram Widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:1110px; height:347px"></iframe>
+  </div>
 </div>
   <div class="container">
     //content
