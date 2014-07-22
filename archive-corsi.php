5c0fe155913afa35ae00b436529e21763c341d21
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2014-07-22 14:25:46 +0200

    category page

--- archive-corsi.php
+++ archive-corsi.php
@@ -8,48 +8,49 @@ get_header(); ?>
   <div class="row">
     <h1 id="corsi-title">Corsi <span class="amp">&amp;</span> laboratori</h1>
     <p>
-      Me non paenitet nullum festiviorem excogitasse ad hoc. Contra legem facit qui id facit quod lex prohibet. Quis aute iure reprehenderit in voluptate velit esse. Magna pars studiorum, prodita quaerimus. Sed haec quis possit intrepidus aestimare tellus.
-      Idque Caesaris facere voluntate liceret: sese habere. Pellentesque habitant morbi tristique senectus et netus. Unam incolunt Belgae, aliam Aquitani, tertiam. Quo usque tandem abutere, Catilina, patientia nostra?
+      <?php
+        /*
+         * Get corsi categori description
+         */ 
+        $corsi = get_category_by_slug('corsi'); 
+        $desc = $corsi->description;
+        echo $desc;
+      ?>
     </p>
   </div>
   <div class="row">
-    <div class="col-sm-12 corso-tile">
-      <div class="row">
-        <div class="corso-picture">
-          <img src="<?php bloginfo( 'template_url' ); ?>/img/corso-bambini.jpg">
-        </div>
-        <div class="corso-caption">
-          <h2 class="corso-title">Scuole</h2>
-          <p>
-            Unam incolunt Belgae, aliam Aquitani, tertiam. Excepteur sint obcaecat cupiditat non proident culpa. Etiam habebis sem dicantur magna mollis euismod. Quis aute iure reprehenderit in voluptate velit esse. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.
-          </p>
-          <p class="text-right">
-            <a href="<?php echo home_url(); ?>/category/scuole">
-              <button class="tile-informazioni"><em>Ulteriori informazioni</em></button>
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-12 corso-tile">
-      <div class="row">
-        <div class="corso-picture">
-          <img src="<?php bloginfo( 'template_url' ); ?>/img/corso-adulti.jpg">
-        </div>
-        <div class="corso-caption">
-          <h2 class="corso-title">Per tutti</h2>
-          <p>
-            Unam incolunt Belgae, aliam Aquitani, tertiam. Excepteur sint obcaecat cupiditat non proident culpa. Etiam habebis sem dicantur magna mollis euismod. Quis aute iure reprehenderit in voluptate velit esse. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.
-          </p>
-          <p class="text-right">
-            <a href="<?php echo home_url(); ?>/category/per-tutti">
-              <button class="tile-informazioni"><em>Ulteriori informazioni</em></button>
-            </a>
-          </p>
+
+    <?php
+      /*
+       * Get subcategory information
+       */ 
+      $corsi_id = $corsi->term_id;
+      $categories =  get_categories('child_of='.$corsi_id.'&hide_empty=0');
+      foreach ($categories as $category) :
+      ?>
+      <div class="col-sm-12 corso-tile">
+        <div class="row">
+          <div class="corso-picture">
+            <img src="<?php bloginfo( 'template_url' ); ?>/img/<?php echo $category->slug; ?>.jpg">
+          </div>
+          <div class="corso-caption">
+            <h2 class="corso-title"><?php echo $category->name; ?></h2>
+            <p>
+              <?php echo $category->description; ?>   
+            </p>
+            <p class="text-right">
+              <a href="<?php echo home_url(); ?>/category/<?php echo $category->slug; ?>">
+                <button class="tile-informazioni"><em>Ulteriori informazioni</em></button>
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+      
+    <?php
+      endforeach;
+    ?>
+
 </div>
 
 <?php get_footer(); ?>
